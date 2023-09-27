d5eb6b7ca42619e3b5b9355fb3a60bf2c3220877
matutul <ashrafujjamantutul@gmail.com>
2023-09-27 21:40:55 +0600

    first commit

---
+++ src/main/java/com/tutul/databaseConnection/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.tutul.databaseConnection.controller;
+
+import com.tutul.databaseConnection.exception.UserNotFoundException;
+import com.tutul.databaseConnection.model.User;
+import com.tutul.databaseConnection.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:5173")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id).orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setName(newUser.getName());
+                    user.setUsername(newUser.getUsername());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
+    }
+}
---
+++ src/main/java/com/tutul/databaseConnection/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.tutul.databaseConnection.repository;
+
+import com.tutul.databaseConnection.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
